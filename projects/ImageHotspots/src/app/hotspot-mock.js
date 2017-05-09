e72fcbffc4570a3b326789bba25adb6166f49898
Bùi Ngọc Sơn <sonbn.225@gmail.com>
2017-05-09 22:49:05 +0700

    Update gitignore and gitignore is now working

--- projects/ImageHotspots/src/app/hotspot-mock.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HOTSPOTS = [
-    { id: 1, x: 200, y: 200 },
-    { id: 2, x: 300, y: 300 },
-];
-//# sourceMappingURL=hotspot-mock.js.map
--- projects/ImageHotspots/src/app/popup.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PopUp = (function () {
-    function PopUp() {
-    }
-    return PopUp;
-}());
-exports.PopUp = PopUp;
-//# sourceMappingURL=popup.js.map
