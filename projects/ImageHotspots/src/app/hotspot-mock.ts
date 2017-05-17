9e7aa5b78636a1aa3db2e963d4908c1272c2f6bc
Bùi Ngọc Sơn <sonbn.225@gmail.com>
2017-05-17 16:29:49 +0700

    Update background

--- projects/ImageHotspots/src/app/hotspot-mock.ts
+++ projects/ImageHotspots/src/app/hotspot-mock.ts
@@ -2,6 +2,7 @@
 import { HotSpot } from './hotspot';
 
 export const HOTSPOTS: HotSpot[] = [
-  {id: 1, x: 200, y: 200},
-  {id: 2, x: 300, y: 300},
+  {id: 1, x: 20, y: 20},
+  {id: 2, x: 30, y: 30},
+  {id: 3, x: 50, y: 60}
 ];
