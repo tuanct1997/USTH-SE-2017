bacd2bf23530b90c74c602c22a0d4a5954bb361e
NLag <NLag@users.noreply.github.com>
2017-05-19 05:58:24 +0700

    add popup

--- projects/Interactive Video/src/app.ts
+++ projects/Interactive Video/src/app.ts
@@ -5,7 +5,7 @@ import {bootstrap,Component,FORM_DIRECTIVES,NgClass,NgFor,NgIf} from 'angular2/a
     template: `
 <div >
   <h1> ANLLELA SAGRA - WORKOUT MOTIVATION </h1>
-  <div class="vidplay"  >
+  <div class="vidplay" >
     
     <video class="vid" id="mp" poster="./video/ANLLELA-SAGRA-WORKOUT-MOTIVATION-thumbnail.png" (loadedmetadata)="processMetaData(movieplayer)"  #movieplayer (timeupdate)="updateDisplayTime(movieplayer)" (mouseenter)= "hideControl(false)" (mouseout)= "hideControl(true)" >
       <source src="./video/ANLLELA-SAGRA-WORKOUT-MOTIVATION.mp4"/>
@@ -20,10 +20,57 @@ import {bootstrap,Component,FORM_DIRECTIVES,NgClass,NgFor,NgIf} from 'angular2/a
       <span style="color:white">{{currTime}} / {{videoDuration}}</span>
       <button (click)="makeFullScreen(movieplayer)" style="float:right;"><img style="width:16px;" src="./fullscreen.svg"></button>
     </div>
-    <div class="popupno1">
+    
+    <div class="popupno1" [hidden]="popup1">
+      <a href="https://www.youtube.com/watch?v=PCZ2JHaPvZ4&t=2s"> Original Video !!! </a>
     </div>
-    <img class="smile" [hidden]="popup1" src="smile.png" alt="Smiley face" height="40" width="40" >
+
+    <div class="popupno2" [hidden]="popup2">
+      <img class="smile" src="smile.png" alt="Smiley face" height="60" width="60" >
+    </div>
+   
   </div>
+  <div class="popupno3" [hidden]="popup3">
+    <div id="watch-description-content">
+      <div id="watch-description-clip">
+        <div id="watch-uploader-info">
+            <strong class="watch-time-text">Published on Jun 30, 2016</strong>
+        </div>
+        <div id="watch-description-text" class="">
+          <p id="eow-description" class="">
+            ➢ Hire Me As Your Online Personal Trainer: 
+            <a href="http://Anllelasagra.net" class="yt-uix-servicelink  " data-url="http://Anllelasagra.net" data-servicelink="CDEQ6TgYACITCIfi8Li5-tMCFcGUWAodXaEHIij4HQ" data-target-new-window="True" rel="nofollow noopener" target="_blank">
+              http://Anllelasagra.net
+            </a>
+            <br>➢ 1Up Discount code (SAGRA) 
+            <a href="http://www.1upnutrition.com" class="yt-uix-servicelink  " data-url="http://www.1upnutrition.com" data-servicelink="CDEQ6TgYACITCIfi8Li5-tMCFcGUWAodXaEHIij4HQ" data-target-new-window="True" rel="nofollow noopener" target="_blank">
+              http://www.1upnutrition.com
+            </a>
+            <br>➢ Instagram: 
+            <a href="http://instagram.com/Anllela_sagra" class="yt-uix-servicelink  " data-url="http://instagram.com/Anllela_sagra" data-servicelink="CDEQ6TgYACITCIfi8Li5-tMCFcGUWAodXaEHIij4HQ" data-target-new-window="True" rel="nofollow noopener" target="_blank">
+              http://instagram.com/Anllela_sagra
+            </a>
+            <br>➢ Facebook: 
+            <a href="https://www.facebook.com/Anllela-Sagra-466521906818912/" class="yt-uix-servicelink  " data-url="https://www.facebook.com/Anllela-Sagra-466521906818912/" data-servicelink="CDEQ6TgYACITCIfi8Li5-tMCFcGUWAodXaEHIij4HQ" data-target-new-window="True" rel="nofollow noopener" target="_blank">
+              https://www.facebook.com/Anllela-Sagr...
+            </a>
+          </p>
+        </div>  
+        <div id="watch-description-extras">
+          <ul class="watch-extras-section">
+            <li class="watch-meta-item yt-uix-expander-body">
+              <h4 class="title">Category</h4>
+              <ul class="content watch-info-tag-list"><li><a href="/channel/UCEgdi0XIXXZ-qJOFPf4JSKw" class="g-hovercard yt-uix-sessionlink      spf-link " data-ytid="UCEgdi0XIXXZ-qJOFPf4JSKw" data-sessionlink="ei=cxoeWcfcG8Gp4gLdwp6QAg">Sports</a></li></ul>
+            </li>
+            <li class="watch-meta-item yt-uix-expander-body">
+              <h4 class="title">License</h4>
+              <ul class="content watch-info-tag-list"><li>Standard YouTube License</li></ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div> 
 </div>
 `,
     styles: [` 
@@ -49,13 +96,32 @@ h1 {
   width: 70%;
   top: -50px;
   margin: 0 auto;
-}
-.smile {
-  position:relative;
-  top:-200px;
-  left:200px;
   z-index:1;
 }
+.popupno1 {
+  position:absolute;
+  top:100px;
+  left:200px;
+  z-index:1;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size:30px;
+  font-weight:bold;
+  background-color:#ff0000;
+}
+.popupno2 {
+  position:absolute;
+  top:100px;
+  left:800px;
+  z-index:1;
+  background-color:#0066ff;
+}
+.popupno3 {
+  position:relative;
+  z-index:1;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+
 @media screen and (-webkit-min-device-pixel-ratio:0) { 
 
 input[type=range].slider {
@@ -254,6 +320,10 @@ class AppComponent {
     public hidectrl: boolean = true;
     public popup1: boolean =true;
     public popup1done: boolean =false;
+    public popup2: boolean =true;
+    public popup2done: boolean =false;
+    public popup3: boolean =true;
+    public popup3done: boolean =false;
 
     getBrowser() {
         if (navigator.userAgent.indexOf("Chrome") != -1) {
@@ -300,11 +370,37 @@ class AppComponent {
           this.popup1 = false;
           this.popup1done = true;
       }
+      if ((this.currTime == "0:15") && (this.popup1done == true)) {
+          this.popup1 = true;
+          this.popup1done = false;
+      }
+    }
+    checkpopup2(moviePlayer){
+      if ((this.currTime == "0:10") && (this.popup2done == false)) {
+          this.popup2 = false;
+          this.popup2done = true;
+      }
+      if ((this.currTime == "0:15") && (this.popup2done == true)) {
+          this.popup2 = true;
+          this.popup2done = false;
+      }
+    }
+    checkpopup3(moviePlayer){
+      if ((this.currTime == "0:20") && (this.popup3done == false)) {
+          this.popup3 = false;
+          this.popup3done = true;
+      }
+      if ((this.currTime == "3:0") && (this.popup3done == true)) {
+          this.popup3 = true;
+          this.popup3done = false;
+      }
     }
     updateDisplayTime(moviePlayer) {
         this.currTime = this.secondsToMinutesAndSeconds(moviePlayer.currentTime);
         this.currentTimeRange = moviePlayer.currentTime;
         this.checkpopup1(moviePlayer);
+        this.checkpopup2(moviePlayer);
+        this.checkpopup3(moviePlayer);
     }
     onMuteUnmute(moviePlayer){
         if(moviePlayer.muted){
